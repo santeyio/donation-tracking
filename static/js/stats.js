7c08e443d5da17ffe55c1b6c8641f093013302bd
Caleb Hayashida <santeyio@gmail.com>
2017-10-26 02:22:27 -0400

    cleaning up stats page

--- static/js/stats.js
+++ static/js/stats.js
@@ -9,41 +9,6 @@ function firework_show(){
 
 //window.onload = firework_show();
 
-var socket = io.connect('/')
-var stack_bottomleft = {"dir1": "right", "dir2": "up", "push": "top"};
-
-socket.on('donation_update', function(data){
-  stream.$data.monthly_donations[data.user_id] = {
-    amount: data.monthly_donation,
-    renewal: data.renewal,
-    renewal_increase: data.renewal_increase,
-  };
-  stream.$data.one_time_donations[data.user_id] = {
-    amount: data.one_time_donation,
-  };
-  console.log(data);
-  if (Number(data.monthly_donation) > 0){
-    new PNotify({
-      title: 'New Monthly!',
-      text: `<h1>\$${Number(data.monthly_donation)+Number(data.renewal_increase)}</h1>`,
-      type: 'success',
-      addclass: 'stack-bottomleft',
-      stack: stack_bottomleft,
-    });
-  };
-  if (Number(data.one_time_donation) > 0){
-    setTimeout(function(){
-      new PNotify({
-        title: 'New One Time!',
-        text: `<h1>\$${data.one_time_donation}</h1>`,
-        type: 'success',
-        addclass: 'stack-bottomleft',
-        stack: stack_bottomleft,
-      });
-    }, 500);
-  };
-});
-
 var stream = new Vue({
   el: '#stream',
   data: {
@@ -51,30 +16,44 @@ var stream = new Vue({
     },
     one_time_donations: {
     },
-    percent: 0,
+    background_color: "#209618",
+    firework_show: false,
   },
   computed: {
     total: function(){
+      return this.total_one_time + this.total_monthly;
+    },
+    percent: function(){
+      var percent = ((this.total/7000)*100);
+      if (percent>=100){
+        if (!this.firework_show){
+          firework_show();
+          this.firework_show = true;
+        }
+      };
+      return percent;
+    },
+    total_one_time: function(){
       var total = 0;
+      var donations = this.$data.one_time_donations;
       
-      var otd = this.$data.one_time_donations;
-      for (var key in otd){
-        if (!otd.hasOwnProperty(key)) continue;
-        total += Math.round((Number(otd[key].amount)*100) / 1200);
+      for (var key in donations){
+        if (!donations.hasOwnProperty(key)) continue;
+        total += Math.round((Number(donations[key].amount)*100) / 1200);
       }
 
-      var md = this.$data.monthly_donations;
-      for (var key in md){
-        if (!md.hasOwnProperty(key)) continue;
-        total += Number(md[key].amount);
-        total += Number(md[key].renewal_increase);
+      return total;
+    },
+    total_monthly: function(){
+      var total = 0;
+      var donations = this.$data.monthly_donations;
+
+      for (var key in donations){
+        if (!donations.hasOwnProperty(key)) continue;
+        total += Number(donations[key].amount);
+        total += Number(donations[key].renewal_increase);
       }
       
-      var percent = ((total/7000)*100);
-      this.$data.percent = percent;
-      if (percent>=100){
-        firework_show();
-      };
       return total;
     },
   },
@@ -89,3 +68,64 @@ var stream = new Vue({
       });
   },
 })
+
+var socket = io.connect(':5500/')
+var stack_bottomleft = {"dir1": "right", "dir2": "up", "push": "top"};
+
+socket.on('donation_update', function(data){
+  // push new donations into vue
+  var t = Vue.set(
+    stream.$data.monthly_donations, 
+    data.user_id,
+    {
+      amount: data.monthly_donation,
+      renewal: data.renewal,
+      renewal_increase: data.renewal_increase,
+    });
+  var d = Vue.set(
+    stream.$data.one_time_donations,
+    data.user_id,
+    {
+      amount: data.one_time_donation,
+    });
+
+  // if a new monthly donation then pop a notification
+  if (Number(data.monthly_donation) > 0){
+    var type = 'success';
+    var title = 'New Monthly!';
+    if (data.renewal == true){
+      type = 'notice';
+      title = 'Monthly Renewal!';
+    }
+    new PNotify({
+      title: title,
+      text: `<h1>\$${Number(data.monthly_donation)}</h1>`,
+      type: type,
+      addclass: 'stack-bottomleft',
+      stack: stack_bottomleft,
+    });
+    if (Number(data.renewal_increase > 0)){
+      new PNotify({
+        title: "Monthly Increase!",
+        text: `<h1>\$${Number(data.renewal_increase)}</h1>`,
+        type: 'success',
+        addclass: 'stack-bottomleft',
+        stack: stack_bottomleft,
+      });
+    }
+  };
+
+  // if a new one time donation then pop a notification
+  if (Number(data.one_time_donation) > 0){
+    setTimeout(function(){
+      new PNotify({
+        title: 'New One Time!',
+        text: `<h1>\$${data.one_time_donation}</h1>`,
+        type: 'success',
+        addclass: 'stack-bottomleft',
+        stack: stack_bottomleft,
+      });
+    }, 500);
+  };
+});
+
